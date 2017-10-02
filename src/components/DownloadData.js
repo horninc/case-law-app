319c26cc62af3c3dea4bc250ead7570410238234
Kristaps Horns <k.horns@ideaspool.nl>
2017-10-02 10:15:56 +0200

    Update packages, and switch babyparse to papaparse for performance

--- src/components/DownloadData.js
+++ src/components/DownloadData.js
@@ -1,55 +1,57 @@
 import React from 'react';
-import Papa from 'babyparse';
+import Papa from 'papaparse';
 
 class DownloadData extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.dataToCSV = this.dataToCSV.bind(this);
-        this.saveAs = this.saveAs.bind(this);
-        this.onDownload = this.onDownload.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.dataToCSV = this.dataToCSV.bind(this);
+    this.saveAs = this.saveAs.bind(this);
+    this.onDownload = this.onDownload.bind(this);
+  }
 
-    dataToCSV(data) {
-        //TODO: filter columns to download
-        const csvtext = Papa.unparse(data);
-        return csvtext;
-    }
+  dataToCSV(data) {
+    //TODO: filter columns to download
+    const csvtext = Papa.unparse(data);
+    return csvtext;
+  }
 
 
-    // from http://stackoverflow.com/questions/283956/
-    saveAs(uri, filename) {
-        var link = document.createElement('a');
-        if (typeof link.download === 'string') {
-            document.body.appendChild(link); //Firefox requires the link to be in the body
-            link.download = filename;
-            link.href = uri;
-            link.click();
-            document.body.removeChild(link); //remove the link when done
-        } else {
-            location.replace(uri);
-        }
+  // from http://stackoverflow.com/questions/283956/
+  saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+      document.body.appendChild(link); //Firefox requires the link to be in the body
+      link.download = filename;
+      link.href = uri;
+      link.click();
+      document.body.removeChild(link); //remove the link when done
+    } else {
+      location.replace(uri);
     }
+  }
 
-    onDownload() {
-        const data = this.props.data;
-        const csvContent = this.dataToCSV(data);
-        const fileName = "nodes.csv";
-        let blob = new Blob([csvContent], {
-            type: "application/csv"
-        })
-        const url = URL.createObjectURL(blob)
-        this.saveAs(url, fileName)
-    }
+  onDownload() {
+    const data = this.props.data;
+    const csvContent = this.dataToCSV(data);
+    const fileName = "nodes.csv";
+    let blob = new Blob([csvContent], {
+      type: "application/csv"
+    })
+    const url = URL.createObjectURL(blob)
+    this.saveAs(url, fileName)
+  }
 
-    render() {
-        /*<button type="button" onClick={this.onClick}>Load new data</button>*/
-        return (
-            <button onClick={this.onDownload}>
-              Download filtered nodes
-              </button>
-        );
-    }
+  render() {
+    /*<button type="button" onClick={this.onClick}>Load new data</button>*/
+    return ( <
+      button onClick = {
+        this.onDownload
+      } >
+      Download filtered nodes <
+      /button>
+    );
+  }
 }
 
 export default DownloadData;
