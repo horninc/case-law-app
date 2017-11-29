f7d8fda87ce1eae18463c856407d3d69e81daade
Kristaps Horns <kristaps@kristapss-mbp.wireless.unimaas.local>
2017-11-29 17:54:02 +0100

    6000 nodes file as default

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     this.getNrVisibleNodes = this.getNrVisibleNodes.bind(this);
     this.setVisibleNodes = this.setVisibleNodes.bind(this);
     this.deactivate = this.deactivate.bind(this);
-    const data = require('./data/44.json');
+    const data = require('./data/41.json');
     this.state = {
       data: data,
       loading: false,
